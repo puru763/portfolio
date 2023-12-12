3440530941b78b22cc7c9df516786a8d542c0b1c
Purvesh Krishnani <purvesh152001@gmail.com>
2023-12-13 01:07:38 +0530

    .

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,82 +6,66 @@ import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 
 const About = () => {
   return (
-
     <div className="about-box-container">
+      <div className="about-header">About Me</div>
 
-      <div className="about-header">
-        About Me
-      </div>
-
-
-
-
-
-
-
-<div className="about-container">
-      <div className="about-image">
-        <img src={about} alt="" />
-      </div>
-
-      <div className="about-data">
-        <div className="intro">
-          Hi Everyone, My name is  <span className="ah"> Purvesh Krishnani </span>  and I am from   <span className="ah"> Mumbai  </span> . I
-          have completed my post graduation in   <span  className="ah">MSc (Computer Science )</span>  from KC
-          Collge (Churchgate).
+      <div className="about-container">
+        <div className="about-image">
+          <img src={about} alt="" />
         </div>
 
-        <br />
-        <div className="describe">
-          <div className="description-heading">
-            Terms That can describe me apart form Coding :
+        <div className="about-data">
+          <div className="intro">
+            Hi Everyone, My name is{" "}
+            <span className="ah"> Purvesh Krishnani </span> and I am from{" "}
+            <span className="ah"> Mumbai </span> . I have completed my post
+            graduation in <span className="ah">MSc (Computer Science )</span>{" "}
+            from KC Collge (Churchgate).
           </div>
-          <div className="description-point">
-            <div className="description-point-data">
-              <div className="about-icon">
-                <ExitToAppOutlinedIcon />
-              </div>
-              <div className="about-point">Adventurous</div>
-            </div>
 
-            <div className="description-point-data">
-              <div className="about-icon">
-                <ExitToAppOutlinedIcon />
-              </div>
-              <div className="about-point">Cricket</div>
+          <br />
+          <div className="describe">
+            <div className="description-heading">
+              Terms That can describe me apart form Coding :
             </div>
-            <div className="description-point-data">
-              <div className="about-icon">
-                <ExitToAppOutlinedIcon />
+            <div className="description-point">
+              <div className="description-point-data">
+                <div className="about-icon">
+                  <ExitToAppOutlinedIcon />
+                </div>
+                <div className="about-point">Adventurous</div>
               </div>
-              <div className="about-point">Enthusiast</div>
-            </div>
-            <div className="description-point-data">
-              <div className="about-icon">
-                <ExitToAppOutlinedIcon />
+
+              <div className="description-point-data">
+                <div className="about-icon">
+                  <ExitToAppOutlinedIcon />
+                </div>
+                <div className="about-point">Cricket</div>
               </div>
-              <div className="about-point">Adaptable</div>
-            </div>
-
-
-            <div className="description-point-data">
-              <div className="about-icon">
-                <ExitToAppOutlinedIcon />
+              <div className="description-point-data">
+                <div className="about-icon">
+                  <ExitToAppOutlinedIcon />
+                </div>
+                <div className="about-point">Enthusiast</div>
               </div>
-              <div className="about-point">Honest</div>
+              <div className="description-point-data">
+                <div className="about-icon">
+                  <ExitToAppOutlinedIcon />
+                </div>
+                <div className="about-point">Adaptable</div>
+              </div>
+
+              <div className="description-point-data">
+                <div className="about-icon">
+                  <ExitToAppOutlinedIcon />
+                </div>
+                <div className="about-point">Honest</div>
+              </div>
             </div>
-
-
           </div>
-          
         </div>
       </div>
     </div>
-
-    </div>
-
-    
-   
   );
 };
 
--- src/Components/Project/ProjectCart/ProjectCart.jsx
+++ src/Components/Project/ProjectCart/ProjectCart.jsx
@@ -1,177 +1,159 @@
 import React from "react";
 import "./ProjectCart.css";
 import flux from "./flux.jpg";
-import bookify from './Bookify.png'
-import notes from './reactnotes.png'
-import pomodro from './pomodro.png'
+import bookify from "./Bookify.png";
+import notes from "./reactnotes.png";
+import pomodro from "./pomodro.png";
 
 const ProjectCart = () => {
   return (
     <div className="project-data-container">
+      <div className="project-cart-data-heading">
+        <strong>Project 1 </strong>
+      </div>
 
+      <div className="Project-cart-container">
+        <div className="project-cart-data">
+          <div className="project-cart-data-name"> Flux Furniture </div>
+          <div className="project-cart-data-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            reiciendis, doloremque beatae ratione quae animi tempore nulla
+            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
+            nobis blanditiis ducimus est deleniti molestias.
+          </div>
 
+          <div className="project-cart-data-button">
+            <button
+              className="project-sourcecode-btn"
+              onClick={() => (window.location.href = "")}
+            >
+              View Project
+            </button>
+          </div>
+        </div>
+        <div className="project-cart-img">
+          <img className="project-img" src={flux} alt="" />
+        </div>
+      </div>
 
+      <div className="project-cart-data-heading">
+        <strong>Project 2 </strong>
+      </div>
 
-              <div className="Project-cart-container">
-                <div className="project-cart-data">
-                  <div className="project-cart-data-heading">
-                    <strong>Project 1 </strong>
-                  </div>
+      <div className="Project-cart-container">
+        <div className="project-cart-data">
+          <div className="project-cart-data-name"> Flux Furniture </div>
+          <div className="project-cart-data-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            reiciendis, doloremque beatae ratione quae animi tempore nulla
+            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
+            nobis blanditiis ducimus est deleniti molestias.
+          </div>
 
-                  <div className="project-cart-data-name"> Flux Furniture </div>
-                  <div className="project-cart-data-description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                    reiciendis, doloremque beatae ratione quae animi tempore nulla
-                    cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
-                    nobis blanditiis ducimus est deleniti molestias.
-                  </div>
+          <div className="project-cart-data-button">
+            <button
+              className="project-sourcecode-btn"
+              onClick={() => (window.location.href = "")}
+            >
+              View Project
+            </button>
+          </div>
+        </div>
 
-                  <div className="project-cart-data-button">
-                    <button
-                      className="project-sourcecode-btn"
-                      onClick={() => (window.location.href = "")}
-                    >
-                      View Project
-                    </button>
-                  </div>
-                </div>
-                  <div className="project-cart-img">
-                    <img className="project-img" src={flux} alt="" />
-                  </div>
-                </div>
+        <div className="project-cart-img">
+          <img className="project-img" src={flux} alt="" />
+        </div>
+      </div>
 
+      <div className="project-cart-data-heading">
+        <strong>Project 3 </strong>
+      </div>
+      <div className="Project-cart-container">
+        <div className="project-cart-data">
+          <div className="project-cart-data-name"> Bookify </div>
+          <div className="project-cart-data-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            reiciendis, doloremque beatae ratione quae animi tempore nulla
+            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
+            nobis blanditiis ducimus est deleniti molestias.
+          </div>
 
+          <div className="project-cart-data-button">
+            <button
+              className="project-sourcecode-btn"
+              onClick={() => (window.location.href = "")}
+            >
+              View Project
+            </button>
+          </div>
+        </div>
 
+        <div className="project-cart-img">
+          <img className="project-img" src={bookify} alt="" />
+        </div>
+      </div>
 
+      <div className="project-cart-data-heading">
+        <strong>Project 4 </strong>
+      </div>
+      <div className="Project-cart-container">
+        <div className="project-cart-data">
+          <div className="project-cart-data-name"> React Notes App </div>
+          <div className="project-cart-data-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            reiciendis, doloremque beatae ratione quae animi tempore nulla
+            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
+            nobis blanditiis ducimus est deleniti molestias.
+          </div>
 
-              <div className="Project-cart-container">
-                <div className="project-cart-data">
-                  <div className="project-cart-data-heading">
-                    <strong>Project 2 </strong>
-                  </div>
+          <div className="project-cart-data-button">
+            <button
+              className="project-sourcecode-btn"
+              onClick={() =>
+                (window.location.href =
+                  "https://puru763.github.io/React-Notes-App/")
+              }
+            >
+              View Project
+            </button>
+          </div>
+        </div>
 
-                  <div className="project-cart-data-name"> Flux Furniture </div>
-                  <div className="project-cart-data-description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                    reiciendis, doloremque beatae ratione quae animi tempore nulla
-                    cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
-                    nobis blanditiis ducimus est deleniti molestias.
-                  </div>
+        <div className="project-cart-img">
+          <img className="project-img" src={notes} alt="" />
+        </div>
+      </div>
 
-                  <div className="project-cart-data-button">
-                    <button
-                      className="project-sourcecode-btn"
-                      onClick={() => (window.location.href = "")}
-                    >
-                      View Project
-                    </button>
-                  </div>
-                </div>
+      <div className="project-cart-data-heading">
+        <strong>Project 5 </strong>
+      </div>
+      <div className="Project-cart-container">
+        <div className="project-cart-data">
+          <div className="project-cart-data-name"> Pomodro Timer </div>
+          <div className="project-cart-data-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            reiciendis, doloremque beatae ratione quae animi tempore nulla
+            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
+            nobis blanditiis ducimus est deleniti molestias.
+          </div>
 
-                <div className="project-cart-img">
-                  <img className="project-img" src={flux} alt="" />
-                </div>
-              </div>
+          <div className="project-cart-data-button">
+            <button
+              className="project-sourcecode-btn"
+              onClick={() =>
+                (window.location.href =
+                  "https://pomodro-timer-react.netlify.app/")
+              }
+            >
+              View Project
+            </button>
+          </div>
+        </div>
 
-
-
-
-
-                  <div className="Project-cart-container">
-                    <div className="project-cart-data">
-                      <div className="project-cart-data-heading">
-                        <strong>Project 3 </strong>
-                      </div>
-
-                      <div className="project-cart-data-name"> Bookify  </div>
-                      <div className="project-cart-data-description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                        reiciendis, doloremque beatae ratione quae animi tempore nulla
-                        cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
-                        nobis blanditiis ducimus est deleniti molestias.
-                      </div>
-
-                      <div className="project-cart-data-button">
-                        <button
-                          className="project-sourcecode-btn"
-                          onClick={() => (window.location.href = "")}
-                          >
-                          View Project
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="project-cart-img">
-                      <img className="project-img" src={bookify} alt="" />
-                    </div>
-                  </div>
-
-
-
-
-
-                  <div className="Project-cart-container">
-                    <div className="project-cart-data">
-                      <div className="project-cart-data-heading">
-                        <strong>Project 4 </strong>
-                      </div>
-
-                      <div className="project-cart-data-name"> React Notes App </div>
-                      <div className="project-cart-data-description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                        reiciendis, doloremque beatae ratione quae animi tempore nulla
-                        cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
-                        nobis blanditiis ducimus est deleniti molestias.
-                      </div>
-
-                      <div className="project-cart-data-button">
-                        <button
-                          className="project-sourcecode-btn"
-                          onClick={() => (window.location.href = "https://puru763.github.io/React-Notes-App/")}
-                          >
-                          View Project
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="project-cart-img">
-                      <img className="project-img" src={notes} alt="" />
-                    </div>
-                  </div>
-
-
-
-
-
-
-                      <div className="Project-cart-container">
-                        <div className="project-cart-data">
-                          <div className="project-cart-data-heading">
-                            <strong>Project 5 </strong>
-                          </div>
-
-                          <div className="project-cart-data-name"> Pomodro Timer  </div>
-                          <div className="project-cart-data-description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                            reiciendis, doloremque beatae ratione quae animi tempore nulla
-                            cupiditate veniam maiores sint blanditiis recusandae sed. Ut unde,
-                            nobis blanditiis ducimus est deleniti molestias.
-                          </div>
-
-                          <div className="project-cart-data-button">
-                            <button
-                              className="project-sourcecode-btn"
-                              onClick={() => (window.location.href = "https://pomodro-timer-react.netlify.app/")}
-                              >
-                              View Project
-                            </button>
-                          </div>
-                        </div>
-
-                        <div className="project-cart-img">
-                          <img className="project-img" src={pomodro} alt="" />
-                        </div>
-                      </div>
+        <div className="project-cart-img">
+          <img className="project-img" src={pomodro} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
